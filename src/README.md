7fd99723a93243ea4952d90c225cd1202e3bea77
Thomas Skodje <97495733+skodjet@users.noreply.github.com>
2022-01-24 22:50:56 -0700

    Fixed an issue with working directory
    Fixed an issue where the working directory of my local machine and that of users did not line up.

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# Smash Tournament Matches Finder
+ Allows the user to search for Super Smash Bros. Ultimate tournament matches ad displays results in a new screen. Clicking on a link will open the match in a browser.
---
+++ src/VideoFinder/VideoCompiler.java
@@ -0,0 +1,18 @@
+package VideoFinder;
+
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class VideoCompiler extends JFrame {
+	
+	ArrayList<String> textLines = new ArrayList<String>();
+
+	public static void main(String[] args) {
+
+		VideoCompiler vidFinder = new VideoCompiler();
+		SearchWindow inputWindow = new SearchWindow();
+		}
+
+
+
+}
